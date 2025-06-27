97744fbe816a0c0449300f1209aafc1df48aa8c4
ThinhNPCE170008 <thinhnpce170008@fpt.edu.vn>
2025-06-28 02:11:15 +0700

    Creat DBContext JDBC

--- src/main/java/util/DBContext.java
+++ src/main/java/util/DBContext.java
@@ -14,29 +14,62 @@ public class DBContext {
 
     public Connection conn = null;
 
+    // Connect Local
+//    public DBContext() {
+//        try {
+//            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+//            String dbURL = "jdbc:sqlserver://localhost:1433;"
+//                    + "databaseName=FLearn;"
+//                    + "user=sa;"
+//                    + "password=123456;"
+//                    + "encrypt=true;trustServerCertificate=true;";
+//            conn = DriverManager.getConnection(dbURL);
+//
+//            if (conn != null) {
+//                DatabaseMetaData dm = (DatabaseMetaData) conn.getMetaData();
+//                System.out.println("Driver name: " + dm.getDriverName());
+//                System.out.println("Driver version: " + dm.getDriverVersion());
+//                System.out.println("Product name: "
+//                        + dm.getDatabaseProductName());
+//                System.out.println("Product version: "
+//                        + dm.getDatabaseProductVersion());
+//            }
+//        } catch (SQLException ex) {
+//        } catch (ClassNotFoundException ex) {
+//            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+
+    // Conect JDBC
     public DBContext() {
         try {
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            String dbURL = "jdbc:sqlserver://localhost:1433;"
-                    + "databaseName=FLearn;"
-                    + "user=sa;"
-                    + "password=123456;"
+
+            // Lấy thông tin từ biến môi trường (set trên Render)
+            String host = System.getenv("DB_HOST");
+            String port = System.getenv("DB_PORT");
+            String dbName = System.getenv("DB_NAME");
+            String user = System.getenv("DB_USER");
+            String pass = System.getenv("DB_PASS");
+
+            String dbURL = "jdbc:sqlserver://" + host + ":" + port + ";"
+                    + "databaseName=" + dbName + ";"
+                    + "user=" + user + ";"
+                    + "password=" + pass + ";"
                     + "encrypt=true;trustServerCertificate=true;";
+
             conn = DriverManager.getConnection(dbURL);
 
             if (conn != null) {
-                DatabaseMetaData dm = (DatabaseMetaData) conn.getMetaData();
+                DatabaseMetaData dm = conn.getMetaData();
                 System.out.println("Driver name: " + dm.getDriverName());
                 System.out.println("Driver version: " + dm.getDriverVersion());
-                System.out.println("Product name: "
-                        + dm.getDatabaseProductName());
-                System.out.println("Product version: "
-                        + dm.getDatabaseProductVersion());
+                System.out.println("Product name: " + dm.getDatabaseProductName());
+                System.out.println("Product version: " + dm.getDatabaseProductVersion());
             }
         } catch (SQLException ex) {
+            ex.printStackTrace();
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
         }
-
     }
 }
